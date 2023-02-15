c69fcbb34fc9f4b67057864a55fec99789dfec7a
Viktoria Boiko <170492v@gmail.com>
2023-02-15 14:01:28 +0300

    save HomeWork 02

---
+++ HomeWork_02/task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number < 1000)
+{
+    int number2 = number / 10;
+    int number3 = number2 % 10;
+    System.Console.Write("Вторая цифра трехзначного числа = " + number3);
+}
+else
+{
+    System.Console.Write("Введено не трехзначное число");
+}
---
+++ HomeWork_02/task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < 1000; i++)
+{
+    if (number > 1000)
+    {
+        number = number / 10;
+    }
+}
+if (number > 99 && number < 1000)
+{
+    int number1 = number % 10;
+    System.Console.Write("Третья цифра: " + number1);
+}
+else
+{
+    System.Console.Write("третьей цифры нет");
+}
