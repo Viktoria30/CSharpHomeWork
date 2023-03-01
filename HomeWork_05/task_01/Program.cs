1831b66ccf69613b9e1d0419c12858bd0b7c3f2d
Viktoria Boiko <170492v@gmail.com>
2023-03-01 14:40:53 +0300

    save HomeWork05

---
+++ HomeWork_05/task_01/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void SumEvenNumber(int[] array, out int sum)
+{
+    int count = 0;
+    sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    sum = sum + count;
+}
+
+const int SIZE = 5;
+var myArray = GenerateArray(SIZE);
+PrintArray(myArray);
+SumEvenNumber(myArray,out int sum);
+System.Console.WriteLine($"Количество чётных чисел в массиве: {sum}");
---
+++ HomeWork_05/task_02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void SumElement(int[] array, out int sum)
+{
+    sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum += array[i];       
+    }
+}
+
+var myArray = GenerateArray(6, 9,20);
+PrintArray(myArray);
+SumElement(myArray, out int sum);
+System.Console.WriteLine($"Сумма элементов нечётных индексов: {sum}");
