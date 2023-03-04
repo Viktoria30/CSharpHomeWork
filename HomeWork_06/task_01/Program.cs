2c0c3134fa3b618f4e2eb46087acfd7eb98f3274
Viktoria Boiko <170492v@gmail.com>
2023-03-04 20:44:35 +0300

    save HomeWork 06

---
+++ HomeWork_06/task_01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41:** Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+System.Console.WriteLine($"Введите элементы массива: ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int PositivElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше нуля {PositivElement(array)}");
+
---
+++ HomeWork_06/task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double b1 = ReadInt("Введите точку b1: ");
+double k1 = ReadInt("Введите точку k1: ");
+double b2 = ReadInt("Введите точку b2: ");
+double k2 = ReadInt("Введите точку k2: ");
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
